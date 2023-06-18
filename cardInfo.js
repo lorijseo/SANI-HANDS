0bcebda5e43f71365b0ac96e22c03c7666c4f454
lorijseo <lorijseo@gmail.com>
2023-06-17 18:35:24 -0700

    create card database

---
+++ cardInfo.js
@@ -0,0 +1,176 @@
+let cards = [
+    {
+        id:0,
+        name: "Vinegar",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:1,
+        name: "Hydrogen Peroxide",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:2,
+        name: "Bleach",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:3,
+        name: "Ammonia",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:4,
+        name: "Rubbing Alcohol",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:5,
+        name: "Toilet Bowl Cleaner",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:6,
+        name: "Mold Remover",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:7,
+        name: "Baking Soda",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:8,
+        name: "Drain Cleaner",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:9,
+        name: "Oxalic Acid",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:10,
+        name: "Trichloroisocyanuric Acid",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:11,
+        name: "Urine",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:12,
+        name: "Glass Cleaner",
+        type: "Ingredient",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:13,
+        name: "Peracetic Acid",
+        type: "Effect",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:14,
+        name: "Peracetic Acid",
+        type: "Effect",
+        good: "",
+        bad: "",
+        info: ""
+    },
+
+    {
+        id:15,
+        name: "Chloramines",
+        type: "Effect",
+        good: "",
+        bad: "",
+        info: "Poison that is less volatile than Chlorine Gas but it stays in water longer"
+    },
+
+    {
+        id:16,
+        name: "Chlorine Gas",
+        type: "Effect",
+        good: "",
+        bad: "",
+        info: "Yellow poisonous gas that causes coughing, eye and nose irritation, and shortness of breath."
+    },
+
+    {
+        id:17,
+        name: "Phosphine Gas",
+        type: "Effect",
+        good: "",
+        bad: "",
+        info: "Colorless and flammable toxic that causes severe lung irritation and compromises cardiac functions. "
+    },
+
+    {
+        id:18,
+        name: "Nullify",
+        type: "Nullify",
+        good: "",
+        bad: "",
+        info: "Not harmful. Does not clean as intended. The mixture is not compatible and neutralizes its effect.  "
+    },
+
+
+
+
+]
