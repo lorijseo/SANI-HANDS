4a83cd00b9a799429b88d2d4f26406397f53e4cb
lorijseo <lorijseo@gmail.com>
2023-06-19 19:40:57 -0700

    personalize explanation for each ingredient combo

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ const quizBtn3 = question3.querySelector(".quizBtn")
 
 quizBtn1.addEventListener("click", function(e){
     e.preventDefault();
-    displayCorrect(question1);
+    displayExplanation(question1)
 
 })
 
 quizBtn2.addEventListener("click", function(e){
     e.preventDefault();
-    displayCorrect(question2);
+    displayExplanation(question2)
 
 })
 
 quizBtn3.addEventListener("click", function(e){
     e.preventDefault();
-    displayCorrect(question3);
+    displayExplanation(question3)
 
 })
 
@@ -61,15 +61,51 @@ function displayCorrect(parent){
 
     parent.querySelector(".cardsInfo").style.display = 'flex';
     parent.querySelector("#answer").style.display = 'flex';
-    parent.querySelector(".cardsInfo").innerHTML = ` <div class="displayCorrect">${isCorrect}! </div> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, at laboriosam atque aut repudiandae consequatur, vitae neque error aperiam, eius ut iure? Unde ex quam suscipit, ut doloribus necessitatibus voluptatem?</p>
-    `
+    // parent.querySelector(".cardsInfo").innerHTML = ` <div class="displayCorrect">${isCorrect}! </div> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, at laboriosam atque aut repudiandae consequatur, vitae neque error aperiam, eius ut iure? Unde ex quam suscipit, ut doloribus necessitatibus voluptatem?</p>
+    // `
+    return isCorrect;
 }
 
 function displayExplanation(parent){
+    let isCorrect = displayCorrect(parent);
+
+    let ingredientsCollection = parent.getElementsByClassName("ingredientCard");
+
+    //get first name
+    let ingredient1 = ingredientsCollection[0].id;
+    let firstLetter = ingredient1.charAt(0).toUpperCase();
+    ingredient1 = firstLetter + ingredient1.slice(1);
+
+    console.log(ingredient1);
+  
+    console.log(ingredientsCollection[1].id);
+
+    //get second name
+    let ingredient2 = ingredientsCollection[1].id;
+
+    firstLetter = ingredient2.charAt(0).toUpperCase();
+    ingredient2 = firstLetter + ingredient2.slice(1);
+
+    console.log(ingredient2);
+
+    // if there's an underscore
+    if (ingredient2.indexOf("_") !== -1){
+        let underscore = ingredient2.indexOf("_");
+        const secondLetter = ingredient2.charAt(underscore + 1).toUpperCase();
+        ingredient2 = ingredient2.slice(0,9) + secondLetter + ingredient2.slice(10);
+        ingredient2 = ingredient2.replace("_", " ");
+    }
+
+    for (let i=0; i<cardCombo.length;i+=1){
+        let ingredientsList = cardCombo[i].ingredients;
+        if ((ingredientsList.indexOf(ingredient1) !== -1) && (ingredientsList.indexOf(ingredient2) !== -1)){
+            parent.querySelector(".cardsInfo").innerHTML = ` 
+            <div class="displayCorrect">${isCorrect}! </div> <p>${cardCombo[i].explanation}</p>
+            `
+        }
+    }
 
 
-    parent.querySelector(".cardsInfo").innerHTML = ` <div class="displayCorrect">${isCorrect}! </div> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, at laboriosam atque aut repudiandae consequatur, vitae neque error aperiam, eius ut iure? Unde ex quam suscipit, ut doloribus necessitatibus voluptatem?</p>
-    `
 }
 
 
@@ -151,4 +187,4 @@ let cardCombo = [
     },
 
 
-]
+];
