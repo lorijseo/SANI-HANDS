3f7dc0b6e617fa2d07983bffd7e065bf8821a0d2
lorijseo <lorijseo@gmail.com>
2023-07-20 19:51:35 -0700

    create cards

--- script2.js
+++ script2.js
@@ -3,24 +3,78 @@
 
 
 // }
-async function createCard(searchName){
+async function getCardData(searchName){
     console.log("weee")
     const response = await fetch("data.json");
     const data = await response.json();
     for (let i=0; data.cards.length; i++){
         if (data.cards[i].name == searchName){
             console.log(data.cards[i])
+            createCard(data.cards[i])
         }
     }
 
 }
 
-createCard("Vinegar")
+function createCard(cardData){
+    let cardType = cardData.type;
+    let cardName = cardData.name;
+    let formatType = cardType.charAt(0).toLowerCase() + cardType.slice(1);
+    let formatName = cardName.charAt(0).toLowerCase() + cardName.slice(1);
+    console.log(formatType)
 
-// let createBtn = document.getElementById("create")
+    let displayCard = document.querySelector(".creation");
+    displayCard.innerHTML = `
+        <div class="${formatType + "Card"}" id="${formatName}">
+        <div class="cardContent">
+        <p class="${formatType + "Descr"}" id="cardName"><i class="fa-solid fa-flask" style="color: green;"></i>${cardName} </p>
+        <img src="images/product.jpg" alt="">
+        <p class="${formatType + "Descr"}" id="cardType">${cardType}</p>
+        <div class="${formatType + "Descr"}" id="cardDescr">
 
-// createBtn.addEventListener("click", function(e){
-//     e.preventDefault();
-//     createCard(data);
+        </div>
+        </div>
+    </div>
+    `
+    cardDescription(cardData, formatName);
 
-// })
+
+}
+
+function cardDescription(cardData, name){
+    let cardId = "#" + name; 
+    let findCard = document.querySelector(cardId)
+    let displayDescr = findCard.querySelector("#cardDescr")
+    if (cardData.type = "Ingredient"){
+        displayDescr.innerHTML = `
+            <p><span style="padding-right:5px;"></span> <i class="fa-solid fa-thumbs-up" style="color: green;"></i>  ${cardData.good}</p>
+            <p><span style="padding-right:5px;"></span> <i class="fa-solid fa-skull-crossbones" style="color: red"></i>  ${cardData.bad}</p>
+
+        `
+    }
+    elif (cardData.type = "Effect"){
+        displayDescr.innerHTML = `
+            <p><span style="padding-right:5px;"></span> <i class="fa-solid fa-skull-crossbones" style="color: red"></i>  Effects: ${cardData.info} </p>
+        `
+
+    }
+
+    elif (cardData.type = "Product"){
+        displayDescr.innerHTML = `
+            <p><span style="padding-right:5px;"></span> <i class="fa-solid fa-skull-crossbones" style="color: red"></i>  Effects: ${cardData.info} </p>
+        `
+
+    }
+
+}
+
+// function displayCard(cardData, )
+
+let createBtn = document.getElementById("create")
+
+createBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    getCardData("Vinegar")
+
+
+})
