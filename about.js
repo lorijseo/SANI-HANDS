3d68991fc19c645eee2bbac6e5c9fc26dbd5c179
lorijseo <lorijseo@gmail.com>
2023-07-24 22:39:54 -0700

    display email prompt

--- about.js
+++ about.js
@@ -1,4 +1,15 @@
-const emailBtn = document.getElementById(id);
+const emailBtn = document.getElementById("email");
+console.log("weee");
+
 emailBtn.addEventListener("click", function(e){
-    
+    e.preventDefault();
+    console.log("wooo");
+    document.querySelector(".sendEmail").style.display="block";
 })
+
+
+// emailBtn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     console.log("wooo");
+//     document.querySelector("#emailForm").style.display="block";
+// })
