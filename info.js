5fc551f9c24a886ba02b34fae6b15578ac463d9e
lorijseo <lorijseo@gmail.com>
2023-08-03 21:05:12 -0700

    information icon in library

--- info.js
+++ info.js
@@ -281,7 +281,7 @@ window.addEventListener("load", async function(){
 
     const displayCardNumber = document.querySelector("#numOfCards");
     displayCardNumber.innerHTML = `
-    There are <b>${cardsData.length}</b> cards in our database
+    There are <b>${cardsData.length}</b> chemical cards in our database
     `
     for (let i=0; i<cardsData.length; i++){
         const currentData = cardsData[i];
